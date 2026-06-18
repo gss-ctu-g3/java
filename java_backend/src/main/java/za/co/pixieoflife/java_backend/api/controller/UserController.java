8a427defa8695d073a324cd560124f47ebfa4620
Crystal <93846409+PixieOfLife@users.noreply.github.com>
2026-06-18 19:18:53 +0200

    Add files via upload

---
+++ java_backend/src/main/java/za/co/pixieoflife/java_backend/api/controller/UserController.java
@@ -0,0 +1,27 @@
+package za.co.pixieoflife.java_backend.api.controller;
+
+import za.co.pixieoflife.java_backend.api.model.User;
+import za.co.pixieoflife.java_backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping("/user")
+    public User getUser(@RequestParam Integer id){
+        Optional<User> user = userService.getUser(id);
+        return (User) user.orElse(null);
+    }
+}
